b069ea0c1fdb83e6c6576d14e592f4b44935d4ea
tibaiwan <2918442956@qq.com>
2021-02-01 23:57:14 +0800

    up

---
+++ src/02useEffect/Index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { UseStateComp } from './01.js';
+
+export function UseEffectExample() {
+    return (
+        <div>
+            <UseStateComp count={1}/>
+        </div>
+    )
+}
