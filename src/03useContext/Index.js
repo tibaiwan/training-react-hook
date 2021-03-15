353c7514feb1061ab55bbed886d36801d2495a6c
zhangjiang.huang <huang.zhangjiang@mwee.cn>
2021-03-15 22:48:00 +0800

    up

---
+++ src/03useContext/Index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { UseContextComp } from './01.js';
+
+export function UseContextExample() {
+    return (
+        <div>
+            <UseContextComp />
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import { UseStateExample } from './01useState/Index';
 import { UseEffectExample } from './02useEffect/Index';
+import { UseContextExample } from './03useContext/Index';
 
 ReactDOM.render(
   <React.StrictMode>
     {/* <UseStateExample /> */}
-    <UseEffectExample />
+    {/* <UseEffectExample /> */}
+    <UseContextExample />
   </React.StrictMode>,
   document.getElementById('root')
 );
